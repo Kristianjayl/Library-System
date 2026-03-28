5ac664df8d4fe9b3e9c70dba17cdc4011495ce50
Christian <huaxcelogtip@gmail.com>
2026-03-28 23:10:23 +0800

    Just added book_list.html

--- Library_System/views.py
+++ Library_System/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def index(request):
     return render(request, 'Library_System/index.html')
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'Library_System/book_list.html', {'books':books})
